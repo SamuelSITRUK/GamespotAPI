f629ca4f25a7e647f9cf920490ff128a5a1ee676
Samuel Sitruk <samuel.sitruk.galbauddufort@gmail.com>
2025-02-17 16:54:34 +0100

    Create main.py
    Description of the basic functions needed for this project

---
+++ main.py
@@ -0,0 +1,4 @@
+# Function to retrieve the name and dates articles published between 2 dates ont the website GamespotAPI
+
+# Function to retrieve the name games released between 2 dates either on all platforms or on specific(s) platforms
+
