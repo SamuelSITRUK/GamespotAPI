cdb22cb50de765be5f56db43d56442a4e55c0540
Samuel Sitruk <samuel.sitruk.galbauddufort@gmail.com>
2025-02-17 22:48:46 +0100

    Update main.py
    adding of code to have all the names and release dates of the titles to be published between 2 dates

--- main.py
+++ main.py
@@ -8,20 +8,16 @@ import os
 import json
 from datetime import datetime, timedelta
 
-
-
 # gather the API key via the environment variables
 API_KEY=os.environ.get('GAMESTOP_API_KEY')
 
-#####Definition of starting and ending datesfor the construction of the file name
+#Definition of starting and ending dates for the construction of the file name
 end_date='2025-01-29' 
 start_date='2024-12-01' 
 date_name_file= str(start_date + '_' + end_date)
 
-
 BASE_URL = "https://www.gamespot.com/api/articles/?api_key= "+ API_KEY + "&format=json&filter=publish_date:" + start_date + "|" + end_date + "&field_list=publish_date,title&lim=1000000"
 #BASE_URL_ARTICLES = "https://www.gamespot.com/api/articles/"
-
 BASE_URL_releases = "https://www.gamespot.com/api/releases/?api_key="+ API_KEY + "&format=json&filter=publish_date:" + start_date + "|" + end_date + "&field_list=publish_date,title&lim=1000000"
 
 params2_articles = {
@@ -30,7 +26,6 @@ params2_articles = {
 }
 
 response= requests.get(BASE_URL, headers=params2_articles).json() #response object decoded into json(via requests method decoded of json)
-
 
 while response['number_of_total_results'] != response['number_of_page_results'] :
     #print(type(response['number_of_total_results']))
@@ -43,6 +38,31 @@ while response['number_of_total_results'] != response['number_of_page_results'] 
             f.write(i.get('publish_date') + i.get('title') + '\n') 
             print(i.get('publish_date') + i.get('title') + '\n')
         f.write('\n' + '\n')
+
+
+BASE_URL_releases = "https://www.gamespot.com/api/releases/?api_key=" + API_KEY + "&format=json&filter=release_date:" + start_date + "|" + end_date + "&field_list=name,release_date"
+
+params2_releases = {
+        "api_key": API_KEY,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0",              
+}
+
+response_releases= requests.get(BASE_URL_releases, headers=params2_releases).json()
+
+while response_releases['number_of_total_results'] != response_releases['number_of_page_results'] :
+#    last_result_date = 
+    #print(type(response['number_of_total_results']))
+    last_result_date = response_releases['results'][-1] #dernier date avec timeframe
+    time_new= str(datetime.strptime((re.search(regex_date_USA, str(response_releases['results'][-1]))[0]), "%Y-%m-%d %H:%M:%S") + + timedelta(seconds=1))
+    BASE_URL_new = str("https://www.gamespot.com/api/releases/?api_key=" + API_KEY + "&format=json&filter=release_date:"+ time_new + "|" + end_date + "&field_list=release_date,name&lim=1000000")
+    response_releases= requests.get(BASE_URL_new, headers=params2_releases).json() #response object decoded into json(via requests method decoded of json)
+    with open(str("C:\\Users\\samue\\OneDrive\\Bureau\\résumé_releases" + date_name_file + ".txt"), 'a') as f :
+        for i in response_releases['results']:
+            f.write(i.get('release_date') + i.get('name') + '\n') 
+            print(i.get('release_date') + i.get('name') + '\n')
+        f.write('\n' + '\n')
+
+
 
 
 
