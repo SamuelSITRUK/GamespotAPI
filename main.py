3021d2dd8ae6f378e89d8e7bc322dea432e40146
Samuel Sitruk <samuel.sitruk.galbauddufort@gmail.com>
2025-02-17 17:12:38 +0100

    Update main.py
    test of the gathering of the first 100 articles (limitation of the API) published between the dates of 2024-09-18 and 2024-10-01

--- main.py
+++ main.py
@@ -10,3 +10,28 @@ from datetime import datetime, timedelta
 
 
 
+# gather the API key via the environment variables
+API_KEY=os.environ.get('GAMESTOP_API_KEY')
+
+
+BASE_URL = "https://www.gamespot.com/api/articles/?api_key= "+ API_KEY + "&format=json&filter=publish_date:2024-09-18|2024-10-01&field_list=publish_date,title&lim=1000000"
+#BASE_URL_ARTICLES = "https://www.gamespot.com/api/articles/"
+
+BASE_URL_releases = "https://www.gamespot.com/api/releases/?api_key="+ API_KEY + "&format=json&filter=publish_date:2024-09-18|2024-10-01&field_list=publish_date,title&lim=1000000"
+
+
+#&filter=f"publish_date:{start_date.strftime(DATE_FORMAT)}|{end_date.strftime(DATE_FORMAT)}"" 
+DATE_FORMAT = "%Y-%m-%d"
+
+
+params2_articles = {
+        "api_key": API_KEY,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0"
+}
+
+response= requests.get(BASE_URL, headers=params2_articles).json() #response object decoded into json(via requests method decoded of json)
+
+
+
+
+
