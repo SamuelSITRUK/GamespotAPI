9800441b1eb9adfcb857106e275524ddb5448368
Samuel Sitruk <samuel.sitruk.galbauddufort@gmail.com>
2025-02-17 17:30:06 +0100

    Update main.py
    adding of custom dates for the starting and ending dates for the articles

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ from datetime import datetime, timedelta
 # gather the API key via the environment variables
 API_KEY=os.environ.get('GAMESTOP_API_KEY')
 
+#####Definition of starting and ending datesfor the construction of the file name
+end_date='2025-01-29' 
+start_date='2024-12-01' 
+date_name_file= str(start_date + '_' + end_date)
 
-BASE_URL = "https://www.gamespot.com/api/articles/?api_key= "+ API_KEY + "&format=json&filter=publish_date:2024-09-18|2024-10-01&field_list=publish_date,title&lim=1000000"
+
+BASE_URL = "https://www.gamespot.com/api/articles/?api_key= "+ API_KEY + "&format=json&filter=publish_date:" + start_date + "|" + end_date + "&field_list=publish_date,title&lim=1000000"
 #BASE_URL_ARTICLES = "https://www.gamespot.com/api/articles/"
 
-BASE_URL_releases = "https://www.gamespot.com/api/releases/?api_key="+ API_KEY + "&format=json&filter=publish_date:2024-09-18|2024-10-01&field_list=publish_date,title&lim=1000000"
-
-
-#&filter=f"publish_date:{start_date.strftime(DATE_FORMAT)}|{end_date.strftime(DATE_FORMAT)}"" 
-DATE_FORMAT = "%Y-%m-%d"
-
+BASE_URL_releases = "https://www.gamespot.com/api/releases/?api_key="+ API_KEY + "&format=json&filter=publish_date:" + start_date + "|" + end_date + "&field_list=publish_date,title&lim=1000000"
 
 params2_articles = {
         "api_key": API_KEY,
@@ -30,6 +30,10 @@ params2_articles = {
 }
 
 response= requests.get(BASE_URL, headers=params2_articles).json() #response object decoded into json(via requests method decoded of json)
+
+
+
+
 
 
 
